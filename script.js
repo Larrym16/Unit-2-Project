995baa1c7528c5fe3a95a7e436fb45a3f99669fe
LarryCruz <18909476-LarryCruz@users.noreply.replit.com>
2023-05-24 21:28:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let button = document.querySelector('.age-34')
+let age = ""
+button.onclick = () => {
+  age = '34'
+  console.log(age)
+}
+let meetAge
+let belts
+
+handleSubmit = () => {
+  if (age === '34') {
+    
+  }
+}
